f915621bd424932d7b9673249dfeedc045a36076
Greta Tuckute <gretatu@mit.edu>
2024-06-16 12:11:08 -0500

    Create gsn_generalization.py

---
+++ examples/gsn_generalization.py
@@ -0,0 +1,162 @@
+"""
+We want to take 7 subjects, and load their stimulus projections from GSN (computed via load_gsn_results.py) using flag compute_pcs.
+If we have n=20 PCs for each subject, we will have 7 * 20 = 140 projections (200 x 140 matrix).
+For the remaining participant, load their voxel data and separate into trials.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import os
+import glob
+import itertools
+from sklearn.linear_model import LinearRegression
+
+d_uid_to_sess = {'cvn7009': '20231202-ST001',
+                    'cvn7012': '20231202-ST001',
+                    'cvn7002': '20231203-ST001',
+                    'cvn7011': '20231204-ST001',
+                    'cvn7007': '20231204-ST001',
+                    'cvn7006': '20231215-ST001',
+                    'cvn7013': '20240508-ST001',
+                    'cvn7016': '20240530-ST001',}
+
+SUBJECTDIR = '/Users/gt/Library/CloudStorage/GoogleDrive-gretatu@mit.edu/My Drive/Research2020/Sentence7T/FMRI_FSAVERAGE/'
+GSNOUTPUTS = '/Users/gt/Library/CloudStorage/GoogleDrive-gretatu@mit.edu/My Drive/Research2020/Sentence7T/GSN_outputs/'
+PROJOUTPUTDIR = '/Users/gt/Library/CloudStorage/GoogleDrive-gretatu@mit.edu/My Drive/Research2020/Sentence7T/GSN_outputs/outputs/'
+OUTPUTDIR = os.path.join(GSNOUTPUTS, 'gsn_generalization')
+if not os.path.exists(OUTPUTDIR):
+    os.makedirs(OUTPUTDIR)
+
+save = True
+
+flip_sign = True # Load the sign-flipped stimulus projections
+parc = 4
+if parc in [1, 2, 3]:
+    parc_col = 'external_parc'
+elif parc in [4, 5]:
+    parc_col = 'parc_lang'
+uids = ['cvn7009', 'cvn7012', 'cvn7002', 'cvn7011', 'cvn7007', 'cvn7006', 'cvn7013', 'cvn7016']
+
+# For all combinations of 7 train subjects, 1 test subject
+train_uid_combs = list(itertools.combinations(uids, 7))
+
+for train_uids in train_uid_combs:
+    test_uid = list(set(uids) - set(train_uids))
+    print(f'Train: {train_uids}, Test: {test_uid}')
+
+    # For each train subject, load their stimulus projections (200 x 20)
+    train_projs = []
+    for train_uid in train_uids:
+        fname = f'{train_uid}_lh_{parc}_proj_20_flip-{flip_sign}.pkl'
+        projfn = os.path.join(PROJOUTPUTDIR, fname)
+        proj = pickle.load(open(projfn, 'rb'))
+        train_projs.append(proj)
+
+    # Get a matrix of all train projections (200 x 140). Stack them
+    train_projs = np.hstack(train_projs)
+    assert train_projs.shape == (proj.shape[0], len(train_uids) * proj.shape[1])
+
+    # Run SVD on train_projs
+    U, S, VT = np.linalg.svd(train_projs, full_matrices=False)
+    V = VT.T # python returns the transpose
+
+    # scatter of S
+    plt.figure()
+    plt.scatter(range(1, len(S) + 1), S)
+    plt.xlabel('PC')
+    plt.ylabel('Singular Value')
+    plt.show()
+
+    # For the test subject, load their voxel data
+    assert len(test_uid) == 1
+    test_uid = test_uid[0]
+    test_sess = d_uid_to_sess[test_uid]
+    datafn_test = os.path.join(SUBJECTDIR, f'{test_sess}-{test_uid}', 'GLMestimatesingletrialoutputs', 'extracted_voxs',
+                          f'lh_baseline200_{test_sess}-{test_uid}_extracted-voxs-{parc_col}_parcs-{parc}.pkl')
+    d_test = pickle.load(open(datafn_test, 'rb'))
+    data_test = d_test['betas_parc_3d'] # (vertices, stimuli, trials)
+
+    # Load the GSN pickle to get the signal mean (mnS)
+    gsnfn_test = os.path.join(GSNOUTPUTS, f'{test_uid}_lh_{parc}_gsn.pkl')
+    if parc in [1, 2, 3]:
+        # lh_baseline200_20240508-ST001-cvn7013_extracted-voxs-external_parc_parcs-1_permute-False.pkl
+        gsnfn_test = os.path.join(GSNOUTPUTS, f'lh_baseline200_{test_sess}-{test_uid}_extracted-voxs-{parc_col}_parcs-{parc}_permute-False.pkl')
+    elif parc in [4, 5]: # lh_baseline200_20240508-ST001-cvn7013_extracted-voxs-parc_lang_parcs-5_gsn_outputs.pkl
+        gsnfn_test = os.path.join(GSNOUTPUTS, f'lh_baseline200_{test_sess}-{test_uid}_extracted-voxs-{parc_col}_parcs-{parc}_gsn_outputs.pkl')
+
+    results_test = pickle.load(open(gsnfn_test, 'rb'))
+
+    # mnS contains the signal mean per voxel. subtract from data_test
+    mnS_reshaped = results_test['mnS'].reshape(-1, 1, 1)  # reshaping to (vertices, 1, 1)
+    data_test_subtracted = data_test - mnS_reshaped
+
+    # # Separate the data_test_subtracted into test_train_trials (trials 1 and 2) and test_test_trials (trial 3)
+    # test_uid_train_trials = data_test_subtracted[:, :, :2]
+    # test_uid_train_trials = np.mean(test_uid_train_trials, axis=2)     # Average over the two trials
+    # test_uid_test_trials = data_test_subtracted[:, :, 2]
+
+    # Separate the data_test_subtracted into test_train_trials (trials 1) and test_test_trials (trial 2 and 3). Iterate over combinations of trials
+    train_trials = [0, 1, 2]
+    test_trials = [[1, 2], [0, 2], [0, 1]]
+
+    for train_trial, test_trial in zip(train_trials, test_trials):
+        test_uid_train_trials = data_test_subtracted[:, :, train_trial]
+        test_uid_test_trials = data_test_subtracted[:, :, test_trial]
+
+        # Average over the two trials
+        # test_uid_train_trials = np.mean(test_uid_train_trials, axis=2)     # Average over the two trials
+        # test_uid_test_trials = np.mean(test_uid_test_trials, axis=2)     # Average over the two trials
+
+        # Run linear regression on the train trials
+
+    scores_train = []
+    scores_test = []
+    for j in range(0, 20):
+        # Take out the jth PC from U
+        U_j = U[:, :j+1] # design matrix is 200 by j
+
+        reg = LinearRegression(fit_intercept=False).fit(U_j, test_uid_train_trials.T)
+
+        score_test = reg.score(U_j, test_uid_test_trials.T)
+        scores_test.append(score_test)
+
+        score_train = reg.score(U_j, test_uid_train_trials.T)
+        scores_train.append(score_train)
+
+        print(f'PCs: {j+1}, Score: {score_test}')
+
+        # recons = U_j @ reg.coef_.T
+
+    plt.figure()
+    plt.plot(range(1, 21), scores_test, label='Test')
+    plt.plot(range(1, 21), scores_train, label='Train')
+    plt.xlabel('Number of PCs')
+    plt.ylabel('R^2')
+    plt.title(f'Test UID: {test_uid}')
+    plt.legend()
+    plt.show()
+
+
+print('Done!')
+
+
+
+
+
+
+    # ## test plot to viz the subtraction
+    # vertex_index = 6
+    # num_trials = 3
+    # fig, axes = plt.subplots(1, num_trials, figsize=(5 * num_trials, 5))
+    # # Check if only one trial is there to handle subplot indexing
+    # if num_trials == 1:
+    #     axes = [axes]
+    # for i in range(num_trials):
+    #     # Scatter plot for each trial comparing pre and post-subtraction
+    #     axes[i].scatter(data_test[vertex_index, :, i], data_test_subtracted[vertex_index, :, i], alpha=0.5)
+    #     axes[i].set_title(f'Vertex {vertex_index} Trial {i}')
+    #     axes[i].set_xlabel('Pre-Subtraction Values')
+    #     axes[i].set_ylabel('Post-Subtraction Values')
+    #     axes[i].plot([-10, 10], [-10, 10], 'k--')
+    # plt.tight_layout()
+    # plt.show()
